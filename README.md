247d43ebe4b39d7d8f910f7035bb0288e684af36
James Alberto Valencia Caicedo <javalenciacai@gmail.com>
2022-07-05 12:59:35 -0500

    Añadir filtros para loguear requests y responses

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # TestWithRestAssured
+Pruebas de servicios API con Rest Assured
+info Rest Assured https://rest-assured.io/
+
+## Dependencias con gradle
+
+    - Crear proyecto Gradle java con ID IntelliJ IDEA
+    - Verificar que tenga junit como dependencia
+    - Agregar rest-assured como dependencia desde Repository Maven central
+    - agregar hamcrest-all como dependecia para realizar aserciones
+## Primer test
+    - usamos given when then para interactuar con rest assured
+    - given " realiza o dame un" get "aqui le pasamos la uri" then " con este recibimos la respuesta"
+    - luego del then agregamos las aserciones
+    - verificar la documentacion
+    
+    
--- src/test/java/ReqResTests.java
+++ src/test/java/ReqResTests.java
@@ -1,13 +1,26 @@
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class ReqResTests {
 
+    @Before
+    public void setUp(){
+        RestAssured.baseURI = "https://reqres.in";
+        RestAssured.basePath = "/api";
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+    }
+    //primer test basico
     @Test
     public void loginTest(){
-        String response = RestAssured
-                .given()
+        String response = given()
                 .log().all()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
@@ -21,5 +34,32 @@ public class ReqResTests {
                 .asString();
 
         System.out.println(response);
+    }
+
+    //Test mas abreviado
+    @Test
+    public void loginTest2(){
+
+                given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"email\": \"eve.holt@reqres.in\",\n" +
+                        "    \"password\": \"cityslicka\"\n" +
+                        "}")
+                .post("/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue());
+
+    }
+    //Test avanzado
+    @Test
+    public void singleUsers(){
+        given()
+                 .contentType(ContentType.JSON)
+                .get("/users/2")
+                .then()
+                .statusCode(HttpStatus.SC_OK)
+                .body("data.id",equalTo(2));
     }
 }
